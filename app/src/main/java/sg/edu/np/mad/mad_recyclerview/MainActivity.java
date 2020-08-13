25a41ac845a5e1912198d275e8e03e6866b83a37
Primaryheap <s10185319@connect.np.edu.sg>
2020-08-14 00:24:14 +0800

    Practical 5

--- app/src/main/java/sg/edu/np/mad/mad_recyclerview/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/mad_recyclerview/MainActivity.java
@@ -1,20 +1,51 @@
 package sg.edu.np.mad.mad_recyclerview;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements TodoAdapter.onTaskClickListener{
+    ArrayList<String> tasklist = new ArrayList<String>(Arrays.asList("Take out the trash", "drink water"));
+    RecyclerView recyclerView;
+    EditText addTaskEdit;
+    Button addTaskBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        TodoAdapter todoAdapter = new TodoAdapter(tasklist , this);
+        recyclerView.setAdapter(todoAdapter);
+
+        addTaskEdit = findViewById(R.id.addtaskedit);
+        addTaskBtn = findViewById(R.id.button);
+        addTaskBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                tasklist.add(addTaskEdit.getText().toString().trim());
+                showNewEntry(recyclerView , tasklist);
+            }
+        });
+
     }
 
     /**
@@ -32,4 +63,37 @@ public class MainActivity extends AppCompatActivity {
         InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(rv.getWindowToken(), 0);
     }
+
+    @Override
+    public void onCardClick(final int position) {
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogLayout = inflater.inflate(R.layout.deletedialog, null);
+
+
+        TextView txtMessage = dialogLayout.findViewById(R.id.dialogmessage);
+        txtMessage.setText(Html.fromHtml( "<div>Are you sure you want to delete<br/>"+ "<b>" + tasklist.get(position) + "?</b></div>"));
+
+        ImageView imgTrash = dialogLayout.findViewById(R.id.dialogicon);
+        imgTrash.setImageResource(R.drawable.ic_baseline_delete_outline_24);
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        tasklist.remove(position);
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .setCancelable(false)
+                .setView(dialogLayout)
+                .show();
+    }
+
+
 }
